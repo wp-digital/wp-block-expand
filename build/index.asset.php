ec36bc9760c3c8eab6e2015d5b380c7088ab80e2
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2023-06-20 17:12:39 +0300

    add icon

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'c44b2b6075fda122facf');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '4ca93533fe9d324bd035');
--- src/icon.js
+++ src/icon.js
@@ -2,6 +2,6 @@ import { SVG, Path } from '@wordpress/primitives';
 
 export default () => (
 	<SVG viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
-		<Path d="" />
+		<Path d="M0,0v9h36V0H0z M9,17l9,9l9-9H9z M0,32v5h36v-5H0z" />
 	</SVG>
 );
